b6b1d578d693159d227129e22c6d3cae6757593b
kusumandaru <angga.kusumandaru@gmail.com>
2016-10-15 14:01:04 +0700

    change footer company and link to metrasys

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,7 +88,7 @@
 
         <!-- Main Footer -->
         <footer class="main-footer" style="max-height: 100px;text-align: center">
-            <strong>Copyright © 2016 <a href="#">Company</a>.</strong> All rights reserved.
+            <strong>Copyright © 2016 <a href="http://www.metrasys.co.id/">PT Sigma Metrasys Solution </a>.</strong> All rights reserved.
         </footer>
 
     </div>
