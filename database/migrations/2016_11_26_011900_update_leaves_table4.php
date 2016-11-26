c5551e3f4fb89731b4e0f0d58915b02f63419158
kusumandaru <apple@Apples-MacBook-Pro.local>
2016-11-26 09:39:39 +0700

    add type in leave

---
+++ database/migrations/2016_11_26_011900_update_leaves_table4.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class UpdateLeavesTable4 extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('leaves', function ($table) {
+            $table->integer('type')->default(0);
+        });
+    }
+
+}
